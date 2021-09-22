f8e8ee02ad738e9a17747baad98f38f7d22bdb32
denm76 <denm76@gmail.com>
2021-09-22 15:56:45 +0300

    Применены циклы и условия

--- js/script.js
+++ js/script.js
@@ -9,12 +9,32 @@ const personalMovieDB = {
   genres: [],
   privat: false
 };
+let counter = 0;
+while (counter<2) {
+  const a = prompt("Один из последних просмотренных фильмов?", "1"),
+        b = prompt("Насколько оцените его?", "1");
+  if(a!='' && a!=null && b!='' && b!=null && a.length<=50){
+    personalMovieDB.movies[a]=b;
+  }
+  else{
+    alert("Введены неправильные данные, попробуйте ещё раз!");
+    continue;
+  }
+  counter++;
+}
 
-const a = prompt("Один из последних просмотренных фильмов?", "1"),
-      b = prompt("Насколько оцените его?", "1"),
-      c = prompt("Один из последних просмотренных фильмов?", "1"),
-      d = prompt("Насколько оцените его?", "1");
-
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
-
+if(personalMovieDB.count<10)
+{
+  document.write('Мало!');
+}
+else if(personalMovieDB.count>=10 && personalMovieDB.count<=30)
+{
+  document.write('Classic!');
+}
+else if(personalMovieDB.count>30)
+{
+  document.write('Good!');
+}
+else{
+  document.write('Something wrong!');
+}
